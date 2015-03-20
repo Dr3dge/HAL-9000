6fc1fcee470c8b0ed9d63f685b3f02951a069ba2
Whatsmename <whatsmename@gmail.com>
2015-03-20 22:55:36 +1100

    Created separate program to install HAL
    - Added separate installer

---
+++ C_Tutorial_1/Installer/Installer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using IWshRuntimeLibrary;
+
+namespace Installer
+{
+    class Installer
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (System.IO.File.Exists(@"C:\Program Files\HAL-9000\HAL-9000.exe"))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("This program is already installed");
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Directory.CreateDirectory(@"C:\Program Files\HAL-9000");
+                    using (WebClient Client = new WebClient())
+                    {
+                        Client.DownloadFile("https://dl.dropboxusercontent.com/s/t9yj5z980siq2du/HAL-9000.exe?dl=0",
+                            @"C:\Program Files\HAL-9000\HAL-9000.exe");
+                    }
+                    using (WebClient Client = new WebClient())
+                    {
+                        Client.DownloadFile("https://dl.dropboxusercontent.com/s/iquqw73byxcmbsv/Updater.exe?dl=0",
+                            @"C:\Program Files\HAL-9000\Updater.exe");
+                    }
+                    object shDesktop = (object)"Desktop";
+                    WshShell shell = new WshShell();
+                    string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\HAL-9000.lnk";
+                    IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+                    shortcut.Description = "Shortcut for HAL-9000";
+                    shortcut.Hotkey = "Ctrl+Shift+H";
+                    shortcut.TargetPath = @"C:\Program Files\HAL-9000\HAL-9000.exe";
+                    shortcut.Save();
+                    Console.WriteLine();
+                    Console.WriteLine("This program was successfully installed");
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+            }
+            catch
+            {
+            Console.WriteLine("I'm sorry Dave, I'm afraid I can't do that.");
+            }
+        }
+    }
+}
